eccad0e55f18e7d6053b30eea1c4f3926f383870
wherywan <wherywan@163.com>
2020-12-16 12:41:42 +0800

    test wanhongjihgjbg56789101145fgbfcvctest test

--- src/HelloWan.java
+++ src/HelloWan.java
@@ -1,5 +1,5 @@
 public class HelloWan {
     public static void main(String[] args) {
-        System.out.println("wajfdjfdgddkdhgfdjgjgjgdfhdhjgkgjdkg");
+        System.out.println("wajfdjfdgddkdhgfdjgjgjcdcdvgdfhdhjgkgjdkg");
     }
 }
