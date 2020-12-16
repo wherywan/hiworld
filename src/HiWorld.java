8bdb69f4d15656d20da96b48a03d8912785dc9cb
whery <1309911856@qq.com>
2020-12-16 11:12:19 +0800

    test wanhongjihgjbg5678

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,5 @@
 public class HiWorld {
     public static void main(String args[]){
-        System.out.println("Hi wanhobnvye56cbcnnhgngji");
+        System.out.println("Hi wanhobnvye5678cbcnnhgngji");
     }
 }
